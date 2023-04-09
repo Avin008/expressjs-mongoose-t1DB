ab3897dd5c3f2d12f8f048f893a5976405b4403c
Avinash Mahananda <32406268+Avin008@users.noreply.github.com>
2023-04-10 03:16:07 +0530

    feat: added login feature

--- src/controllers/login.controller.ts
+++ src/controllers/login.controller.ts
@@ -1,4 +1,30 @@
+import { userModel } from "../models/user";
+
 const loginController = async (req: any, res: any) => {
+  const { email, password } = await req.body;
+
+  try {
+    const findUser = await userModel.findOne({
+      email: email,
+    });
+
+    if (findUser === null)
+      return res
+        .status(400)
+        .json({ message: "invalid email" });
+
+    if (findUser.password === password)
+      return res.status(200).json({
+        message: `welcome back ${findUser.fullname}`,
+      });
+
+    return res
+      .status(400)
+      .json({ message: "invalid password" });
+  } catch (error) {
+    console.log(error);
+  }
+
   res.json({ message: "login route" });
 };
 
